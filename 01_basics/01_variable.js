e5f71dd80c9dd2a41fb0104f49526a3a8b6288fc
sushilrathod <140347414+sushilrathod@users.noreply.github.com>
2023-12-10 18:35:03 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId= 144553
+let accountEmail="sushil.rathod.1044@gmail.com"
+var accountPassword="12345"
+accountCity="Washim"
+
+//accountId=2;
+accountEmail="rathodsushil198@gmail.com";
+accountPassword="7020646956";
+accountCity="Pune";
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+ 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) we are using node not browser
+
+console.log(3 + 3);
+console.log("Sushil");
+
+let name = "sushil";
+let age = 25;
+let isLoggedIn = false;
+let state = null;
+// number=2 to power 53
+//bigint
+//string=>""
+//boolean=> true/false
+// null=> Standalone value
+// undefined  =>
+// symbol  => unique 
+
+
+
+// object 
+
+console.log(typeof"Sushil");
+
+console.log(typeof undefined);
+console.log(typeof null);
