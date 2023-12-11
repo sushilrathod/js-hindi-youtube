39f85ff5cb61b9ff01e1deefd42e1f23a8594f50
sushilrathod <140347414+sushilrathod@users.noreply.github.com>
2023-12-11 11:38:12 +0000

    datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// Primitive 
+
+// 7 types : String ,Number ,Boolean <null ,undefined ,Symbol , BigInt 
+
+const score = 100;
+const scorevlaue=100.3
+
+const isLoggin = false ;
+const outSideTemp=null;
+let userEmail= undefined;
+const id = Symbol('123');
+const anotherId=Symbol('123');
+
+console.log(id==anotherId);
+
+const bigNumber = 45037503245375n;
+
+//Reference type => (Non Premitive)
+
+// Array , Objects , Functions 
+
+const heros = ["shaktiman","naagraj","doga"];
+
+let  muObj=
+{
+    name: "Sushil",
+    age : 25,
+
+}
+
+const myFunction=function(){
+    console.log("Hello world");
+}
